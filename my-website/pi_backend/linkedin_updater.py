ca4552900907ab97bff9e90703a79249b3e37bbe
chrisapton <chrisapton@gmail.com>
2025-07-10 01:32:48 -0700

    debugging

--- my-website/pi_backend/linkedin_updater.py
+++ my-website/pi_backend/linkedin_updater.py
@@ -23,7 +23,7 @@ def update_linkedin():
 
     options = Options()
     # Add these arguments for running on Raspberry Pi
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     
